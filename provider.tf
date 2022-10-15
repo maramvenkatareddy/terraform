38985afe853dd4bb6c919679ae1897f789bcdf37
maramvenkatareddy <113689196+maramvenkatareddy@users.noreply.github.com>
2022-10-15 12:06:07 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,18 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-  }
-    backend "azurerm" {
-        resource_group_name  = "tfstate"
-        storage_account_name = "adityaterraformbackend"
-        container_name       = "terraformstate"
-        key                  = "terraform2.tfstate"
-    }
 
-}
 
 provider "azurerm" {
   features {}
